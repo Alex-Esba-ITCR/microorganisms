0a5ff9f56f421e8574f76bb88401ff756cc23005
Alexander Esquivel Barrientos <alesquivel@estudiantec.cr>
2023-03-05 16:25:28 -0600

    Config GUI

--- microogranisms/src/GUI.java
+++ microogranisms/src/GUI.java
@@ -1,5 +1,13 @@
-public class GUI {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class GUI {
+    private JPanel panelMain;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setContentPane(new GUI().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
