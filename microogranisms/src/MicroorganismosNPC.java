54852101e324059284bbc005b844f79fa2102f54
Alexander Esquivel Barrientos <alesquivel@estudiantec.cr>
2023-03-06 14:04:52 -0600

    Update MicroorganismosNPC.java
    Atributos necesarios creados con sus max y min definidos. La simulacion del movimiento del microorganismo funciona de forma basica.

--- microogranisms/src/MicroorganismosNPC.java
+++ microogranisms/src/MicroorganismosNPC.java
@@ -1,2 +1,237 @@
+import java.util.Random;
+
 public class MicroorganismosNPC {
+    // Atributos
+    private int id;
+    private int energia;
+    private int vision;
+    private int velocidad;
+    private int edad;
+    private int MAX_ENERGIA = 100;
+    private int MAX_VISION = 5;
+    private int MAX_VELOCIDAD = 10;
+    private int MAX_EDAD = 20;
+    private int MIN_ENERGIA = 0;
+    private int MIN_VISION = 1;
+    private int MIN_VELOCIDAD = 1;
+    private int MIN_EDAD = 0;
+    private int x;
+    private int y;
+
+    // Constructor
+    public MicroorganismosNPC(){
+        Random random = new Random();
+        this.id = random.nextInt(99999);
+        this.energia = 50;
+        this.vision = MIN_VISION;
+        this.velocidad = MIN_VELOCIDAD;
+        this.x = random.nextInt(50);
+        this.y = random.nextInt(50);
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        if (energia > MAX_ENERGIA){
+            energia = MAX_ENERGIA;
+        }
+        if (energia < MIN_ENERGIA){
+            energia = MIN_ENERGIA;
+        }
+        this.energia = energia;
+    }
+
+    public int getVision() {
+        return vision;
+    }
+
+    public void setVision(int vision) {
+        if (vision > MAX_VISION){
+            vision = MAX_VISION;
+        }
+        if (vision < MIN_VISION){
+            vision = MIN_VISION;
+        }
+        this.vision = vision;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        if (velocidad > MAX_VELOCIDAD){
+            velocidad = MAX_VELOCIDAD;
+        }
+        if (velocidad < MIN_VELOCIDAD){
+            velocidad = MIN_VELOCIDAD;
+        }
+        this.velocidad = velocidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad > MAX_EDAD){
+            edad = MAX_EDAD;
+        }
+        if (edad < MIN_EDAD){
+            edad = MIN_EDAD;
+        }
+        this.edad = edad;
+    }
+
+    public int getMAX_ENERGIA() {
+        return MAX_ENERGIA;
+    }
+
+    public void setMAX_ENERGIA(int MAX_ENERGIA) {
+        this.MAX_ENERGIA = MAX_ENERGIA;
+    }
+
+    public int getMAX_VISION() {
+        return MAX_VISION;
+    }
+
+    public void setMAX_VISION(int MAX_VISION) {
+        this.MAX_VISION = MAX_VISION;
+    }
+
+    public int getMAX_VELOCIDAD() {
+        return MAX_VELOCIDAD;
+    }
+
+    public void setMAX_VELOCIDAD(int MAX_VELOCIDAD) {
+        this.MAX_VELOCIDAD = MAX_VELOCIDAD;
+    }
+
+    public int getMAX_EDAD() {
+        return MAX_EDAD;
+    }
+
+    public void setMAX_EDAD(int MAX_EDAD) {
+        this.MAX_EDAD = MAX_EDAD;
+    }
+
+    public int getMIN_ENERGIA() {
+        return MIN_ENERGIA;
+    }
+
+    public void setMIN_ENERGIA(int MIN_ENERGIA) {
+        this.MIN_ENERGIA = MIN_ENERGIA;
+    }
+
+    public int getMIN_VISION() {
+        return MIN_VISION;
+    }
+
+    public void setMIN_VISION(int MIN_VISION) {
+        this.MIN_VISION = MIN_VISION;
+    }
+
+    public int getMIN_VELOCIDAD() {
+        return MIN_VELOCIDAD;
+    }
+
+    public void setMIN_VELOCIDAD(int MIN_VELOCIDAD) {
+        this.MIN_VELOCIDAD = MIN_VELOCIDAD;
+    }
+
+    public int getMIN_EDAD() {
+        return MIN_EDAD;
+    }
+
+    public void setMIN_EDAD(int MIN_EDAD) {
+        this.MIN_EDAD = MIN_EDAD;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "MicroorganismosNPC{" +
+                "id=" + id +
+                ", energia=" + energia +
+                ", vision=" + vision +
+                ", velocidad=" + velocidad +
+                ", edad=" + edad +
+                ", MAX_ENERGIA=" + MAX_ENERGIA +
+                ", MAX_VISION=" + MAX_VISION +
+                ", MAX_VELOCIDAD=" + MAX_VELOCIDAD +
+                ", MAX_EDAD=" + MAX_EDAD +
+                ", MIN_ENERGIA=" + MIN_ENERGIA +
+                ", MIN_VISION=" + MIN_VISION +
+                ", MIN_VELOCIDAD=" + MIN_VELOCIDAD +
+                ", MIN_EDAD=" + MIN_EDAD +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    // Métodos
+    public void moverse(){
+        while (this.energia > 0){
+            Random random = new Random();
+            int direccion = random.nextInt(4);
+            switch (direccion) {
+                case 0 -> {
+                    if (this.x + this.velocidad >= 50) {
+                        break;
+                    }
+                    this.x += random.nextInt(this.velocidad + 1);
+                    this.energia -= 1;
+                }
+                case 1 -> {
+                    if (this.x - this.velocidad < 0) {
+                        break;
+                    }
+                    this.x -= random.nextInt(this.velocidad + 1);
+                    this.energia -= 1;
+                }
+                case 2 -> {
+                    if (this.y + this.velocidad >= 50) {
+                        break;
+                    }
+                    this.y += random.nextInt(this.velocidad + 1);
+                    this.energia -= 1;
+                }
+                case 3 -> {
+                    if (this.y - this.velocidad < 0) {
+                        break;
+                    }
+                    this.y -= random.nextInt(this.velocidad + 1);
+                    this.energia -= 1;
+                }
+            }
+        }
+    }
+
 }
